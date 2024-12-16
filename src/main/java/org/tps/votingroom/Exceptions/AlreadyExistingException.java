d398bf47d822517114f8f80bdd167c755acfe7b4
ArtemSmit21 <smarde2103@mail.ru>
2024-12-16 19:45:27 +0300

    Delete src/main/java/org/tps/votingroom/Exceptions directory

--- src/main/java/org/tps/votingroom/Exceptions/AlreadyExistingException.java
+++
@@ -1,7 +0,0 @@
-package org.tps.votingroom.Exceptions;
-
-public class AlreadyExistingException extends Exception {
-  public AlreadyExistingException(String message) {
-    super(message);
-  }
-}
--- src/main/java/org/tps/votingroom/Exceptions/EndVotingException.java
+++
@@ -1,7 +0,0 @@
-package org.tps.votingroom.Exceptions;
-
-public class EndVotingException extends RuntimeException {
-  public EndVotingException(String message) {
-    super(message);
-  }
-}
