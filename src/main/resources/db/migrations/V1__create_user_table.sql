69ac0bbb2abfa074d62b11c98fb7c174826e0d36
ArtemSmit21 <smarde2103@mail.ru>
2025-01-18 13:34:18 +0300

    Delete src/main/resources/db/migrations/V1__create_user_table.sql

--- src/main/resources/db/migrations/V1__create_user_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "user"
-(
-    profile_id BIGINT PRIMARY KEY,
-    firstname VARCHAR(20) NOT NULL,
-    lastname VARCHAR(30) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    friends_id integer[]
-)
