e61c4b4575c12eec53d546b0ec276655d7a6fd9c
pr0stSasha <108032663+pr0stSasha@users.noreply.github.com>
2025-01-19 22:23:17 +0300

    Update AddingVotingRoom.java

--- src/main/java/org/example/AddingVotingRoom.java
+++ src/main/java/org/example/AddingVotingRoom.java
@@ -8,13 +8,15 @@ import java.util.Arrays;
 
 public class AddingVotingRoom {
 
-    public static void addVoting(String owner_name, String date_start, String date_finish, String[] friends_names, String[] trip_variants) throws SQLException{
+    public static void addVoting(String owner_name, String date_start, String date_finish, String[] friends_names, String[] trip_variants) throws SQLException {
         String SQL_QUERY = "INSERT INTO vote (name_owner, date_start, date_finish, name_friends, trip_variants) " +
                 "VALUES (" + owner_name + ", " + date_start + ", " + date_finish + ", ARRAY" + Arrays.toString(friends_names) +
                 ", ARRAY" + Arrays.toString(trip_variants) + ");";
-        try (Connection con = DataSource.getConnection(); PreparedStatement pst = con.prepareStatement( SQL_QUERY );
+        try (Connection con = DataSource.getConnection(); PreparedStatement pst = con.prepareStatement(SQL_QUERY);
              ResultSet rs = pst.executeQuery()) {
+            pst.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
-
 }
