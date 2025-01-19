be966aa24864824b38c1c3b9bf3f7b8d02a7d68f
pr0stSasha <108032663+pr0stSasha@users.noreply.github.com>
2025-01-20 02:25:12 +0300

    Update AddingVotingRoom.java

--- src/main/java/org/example/AddingVotingRoom.java
+++ src/main/java/org/example/AddingVotingRoom.java
@@ -1,22 +1,28 @@
 package org.example;
 
+import java.sql.Array;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Arrays;
 
 public class AddingVotingRoom {
-
-    public static void addVoting(String owner_name, String date_start, String date_finish, String[] friends_names, String[] trip_variants) throws SQLException {
-        String SQL_QUERY = "INSERT INTO vote (name_owner, date_start, date_finish, name_friends, trip_variants) " +
-                "VALUES (" + owner_name + ", " + date_start + ", " + date_finish + ", ARRAY" + Arrays.toString(friends_names) +
-                ", ARRAY" + Arrays.toString(trip_variants) + ");";
-        try (Connection con = DataSource.getConnection(); PreparedStatement pst = con.prepareStatement(SQL_QUERY);
-             ResultSet rs = pst.executeQuery()) {
-            pst.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
+    public static void addVoting(String owner_name, String date_finish,
+                                 String[] friends_names,
+                                 String[] trip_link, String[] trip_name) throws SQLException {
+        String SQL = "INSERT INTO vote (name_owner, date_finish, name_friends, trip_link, trip_name) " + "VALUES (?, ?, ?, ?, ?)";
+        try (Connection con = DataSource.getConnection();
+             PreparedStatement pst = con.prepareStatement(SQL)) {
+            pst.setString(1, owner_name);
+            pst.setString(2, date_finish);
+            Array friendsArray = con.createArrayOf("text", friends_names);
+            pst.setArray(3, friendsArray);
+            Array trip_links_Array = con.createArrayOf("text", trip_link);
+            pst.setArray(4, trip_links_Array);
+            Array trip_names_Array = con.createArrayOf("text", trip_name);
+            pst.setArray(5, trip_names_Array);
+                pst.executeUpdate();
+            } catch (SQLException e) {
+            e.printStackTrace(); // выводим стек ошибки
         }
     }
 }
