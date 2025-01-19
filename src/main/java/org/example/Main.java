d87e8b04e712bf49eb3ec9e80450dd70d8f2b006
pr0stSasha <108032663+pr0stSasha@users.noreply.github.com>
2025-01-20 02:26:10 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,49 +1,81 @@
 package org.example;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
+import com.google.gson.*;
 import freemarker.cache.ClassTemplateLoader;
 import freemarker.template.Configuration;
 import spark.ModelAndView;
 import spark.template.freemarker.FreeMarkerEngine;
 
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
-import static spark.Spark.get;
-import static spark.Spark.post;
+import static spark.Spark.*;
 
 public class Main {
 
     private static final Gson gson = new Gson();
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
 
+        // Настройка шаблонизатора FreeMarker
         FreeMarkerEngine freeMarkerEngine = new FreeMarkerEngine();
-        Configuration freeMarkerConfiguration = new Configuration();
+        Configuration freeMarkerConfiguration = new Configuration(Configuration.VERSION_2_3_21);
         freeMarkerConfiguration.setTemplateLoader(new ClassTemplateLoader(Main.class, "/"));
         freeMarkerEngine.setConfiguration(freeMarkerConfiguration);
 
+        // Отдаём HTML-страницу (шаблон) при GET запросе
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             model.put("title", "Создание голосования");
-            model.put("friends", new String[]{"biba", "boba"});
             return freeMarkerEngine.render(new ModelAndView(model, "template/VotingRoomText.ftl"));
         });
 
-        // Обработка POST-запроса при нажатии на кнопку
+        // При клике "Создать голосование" идёт JSON POST на /AddData
         post("/AddData", (req, res) -> {
-            res.type("application/json");
+            // Парсим JSON из тела запроса
             JsonObject json = gson.fromJson(req.body(), JsonObject.class);
-            String date_start =  ((char) 39) + json.get("date_start").getAsString() + ((char) 39);
-            String date_finish =  ((char) 39) + json.get("date_finish").getAsString() + ((char) 39);
-            String owner_name = ((char) 39)+"Sasha"+ ((char) 39); // пока нет возможности брать имя юзера, поэтому овнер - я
-            String[] friends_names = { ((char) 39) + json.get("friends_names").getAsString() + ((char) 39)};
-            String[] trip_variants =  { ((char) 39) + json.get("trip_variants").getAsString() + ((char) 39) };
-            AddingVotingRoom.addVoting(owner_name,date_start,date_finish,friends_names,trip_variants);
+
+            String dateFinish = json.get("date_finish").getAsString();
+
+            // В вашем случае owner_name - всегда "Sasha"
+            String ownerName = "Sasha";
+
+            // Считываем массив друзей
+            JsonArray friendsArray = json.getAsJsonArray("friends_names");
+            String[] friends = new String[friendsArray.size()];
+            for (int i = 0; i < friendsArray.size(); i++) {
+                friends[i] = friendsArray.get(i).getAsString();
+            }
+
+            // Считываем массив путешествий
+            JsonArray trip_links_array = json.getAsJsonArray("trip_links");
+            String[] trip_links = new String[trip_links_array.size()];
+            for (int i = 0; i < trip_links_array.size(); i++) {
+                trip_links[i] = trip_links_array.get(i).getAsString();
+            }
+
+            JsonArray trip_names_array = json.getAsJsonArray("trip_name");
+            String[] trip_names = new String[trip_names_array.size()];
+            for (int i = 0; i < trip_names_array.size(); i++) {
+                trip_names[i] = trip_names_array.get(i).getAsString();
+            }
+            // Добавляем в БД
+            try {
+                AddingVotingRoom.addVoting(ownerName, dateFinish, friends, trip_links, trip_names);
+            } catch (SQLException e) {
+                e.printStackTrace();
+                // Можно установить код ошибки и вернуть JSON
+                res.status(500);
+                return gson.toJson(Collections.singletonMap("error", "Не удалось создать голосование."));
+            }
+
+            // По желанию можно сделать редирект на /Success или вернуть JSON-ответ
+            // Допустим делаем редирект:
             res.redirect("/Success");
-            return true;
+            return null;
         });
+
+        // Допустим сделаем ручку для /Success
+        get("/Success", (req, res) -> "Голосование успешно создано!");
     }
 }
